b1826067ec993f989aebc020ca594e35ea6b67f3
subbni <slfkak@naver.com>
2023-05-05 01:11:07 +0900

    [fix] proxy 오류 제거

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Sign from "./lettrip-web/Header/Sign"; // ~:8080/Sign-up 회원가입 화
 
 import TravelList from "./lettrip-web/Service/Travel/TravelList"; // ~: /Travel/create 코스 등록 화면
 import CourseCreate from "./lettrip-web/Service/Travel/TravelCourse/CourseCreate"; // ~: /Travel/create/course 코스 등록 계획 화면
-import ReviewCreate from "./lettrip-web/Service/Travel/TravelReview/TravelCreate"; // ~: /Travel/create/review 코스 후기 등록 화면
+// import ReviewCreate from "./lettrip-web/Service/Travel/TravelReview/TravelCreate"; // ~: /Travel/create/review 코스 후기 등록 화면
 
 import ArticlesList from "./lettrip-web/Service/Articles/ArticlesList"; // ~:8080/Articles 게시판 전체 목록 화면
 import ArticlesCreate from "./lettrip-web/Service/Articles/ArticlesCreate"; // ~:8080/Articles/create 게시판 글 등록 화면
@@ -21,25 +21,22 @@ import ArticlesPage from "./lettrip-web/Service/Articles/ArticlesPage"; // ~:808
 function App() {
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Header />
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/Sign-up' element={<Sign />} />
-
-          <Route path='/Travel/create' element={<TravelList />} />
-          <Route path='/Travel/create/course' element={<CourseCreate />} />
-          <Route path='/Travel/create/review' element={<ReviewCreate />} />
-
-          <Route path='/Articles' element={<ArticlesList />} />
-          <Route path='/Articles/create' element={<ArticlesCreate />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Sign-up" element={<Sign />} />
+          <Route path="/Travel/create" element={<TravelList />} />
+          <Route path="/Travel/create/course" element={<CourseCreate />} />
+          <Route path="/Articles" element={<ArticlesList />} />
+          <Route path="/Articles/create" element={<ArticlesCreate />} />
           <Route
-            path='/Articles/modify/{article-id}'
+            path="/Articles/modify/:article-id"
             element={<ArticlesModify />}
           />
-          <Route path='/Articles/page' element={<ArticlesPage />} />
+          <Route path="/Articles/page" element={<ArticlesPage />} />
         </Routes>
       </div>
     </Router>
