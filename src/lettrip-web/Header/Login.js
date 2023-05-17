135f1b55394c3b72f1565966c7a5317d6505d47a
subbni <slfkak@naver.com>
2023-05-17 19:39:41 +0900

    backend 소통 기능 수정

--- src/lettrip-web/Header/Login.js
+++ src/lettrip-web/Header/Login.js
@@ -1,136 +1,69 @@
-import axios from "axios";
-import jwt_decode from "jwt-decode";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-const instance = axios.create({
-  headers: { "Access-Control-Allow-Origin": "*" },
-  baseURL: "http://ec2-3-35-173-250.ap-northeast-2.compute.amazonaws.com:8080",
-  withCredentials: false,
-});
+import { login } from "../Service/APIService";
+import { ACCESS_TOKEN } from "../constant/backendAPI";
 
 function Login() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
 
-  // access token 가져오기
-  function getAccessToken() {
-    return localStorage.getItem("accessToken");
-  }
-  // access token 로컬 스토리지에 저장
-  function setAccessToken(token) {
-    localStorage.setItem("accessToken", token);
-  }
-  // access token 삭제
-  function removeAccessToken() {
-    localStorage.removeItem("accessToken");
-  }
-
-  // 새로고침 이벤트 감지
-  useEffect(() => {
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
-
-  const handleBeforeUnload = () => {
-    localStorage.removeItem("accessToken");
+  const handleLoginFormChange = (e) => {
+    const changedField = e.target.name;
+    setLoginForm({
+      ...loginForm,
+      [changedField]: e.target.value,
+    });
   };
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
   const handleLoginFormSubmit = (e) => {
     e.preventDefault();
-
-    // 서버로 데이터 보낼 준비
-    const data = {
-      username,
-      password,
-    };
-
-    // API call 만들기
-    instance
-      .post("/login", data)
+    login(loginForm)
       .then((response) => {
-        console.log("인증 성공");
-        const token = response.data.accessToken; // 응답에 포함된 토큰 정보 저장
-
-        // 토큰 정보 검증
-        const decodedToken = jwt_decode(token);
-        const now = new Date().getTime() / 1000; // 초 단위 시간 계산
-        if (decodedToken.exp < now) {
-          console.error("토큰 만료");
-          window.alert("세션이 만료되었습니다. 다시 로그인해주세요.");
-          return;
-        }
-        if (
-          decodedToken.iss !==
-          "http://ec2-3-35-173-250.ap-northeast-2.compute.amazonaws.com:8080"
-        ) {
-          console.error("조작된 토큰");
-          window.alert("올바르지 않은 인증 정보입니다. 다시 로그인해주세요.");
-          return;
-        }
-        // 토큰 정보를 로컬 스토리지에 저장
-        localStorage.setItem("accessToken", token);
-        navigate("/");
-      })
-      .catch((error) => {
-        console.error("로그인 검증 실패:", error);
-        window.alert("로그인에 실패했습니다. 다시 확인해주세요.");
+        setJwtToken(response);
+        window.alert("로그인 되었습니다.");
         window.location.reload();
+        navigate(-1);
+      })
+      .catch((e) => {
+        console.log(e);
+        window.alert("로그인에 실패하였습니다. 다시 시도해주시길 바랍니다.");
       });
   };
 
-  // 모든 HTTP 응답에 대해 처리하는 interceptors 설정
-  instance.interceptors.response.use(
-    (response) => {
-      // 상태 코드가 200인 경우
-      return response;
-    },
-    (error) => {
-      // 상태 코드가 200이 아닌 경우
-      console.error("에러 발생:", error);
-      if (error.response.status === 401) {
-        // 인증 실패
-        removeAccessToken(); // 토큰 삭제
-        navigate("/login"); // 로그인 페이지로 이동
-      }
-      return Promise.reject(error);
-    }
-  );
+  const setJwtToken = (response) => {
+    localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+  };
 
   return (
-    <form className='Login_container' onSubmit={handleLoginFormSubmit}>
-      <label htmlFor='username'>아이디</label>
+    <form className="Login_container" onSubmit={handleLoginFormSubmit}>
+      <label htmlFor="email">아이디</label>
       <input
-        className='Login_username'
-        type='text'
-        id='username'
-        name='username'
-        placeholder='아이디를 입력하세요'
+        className="Login_username"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="이메일을 입력하세요"
         required
-        value={username}
-        onChange={handleUsernameChange}
+        value={loginForm.email}
+        onChange={handleLoginFormChange}
       />
-      <label htmlFor='password'>비밀번호</label>
+      <label htmlFor="password">비밀번호</label>
       <input
-        className='Login_password'
-        type='password'
-        id='password'
-        name='password'
-        placeholder='비밀번호를 입력하세요'
+        className="Login_password"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="비밀번호를 입력하세요"
         required
-        value={password}
-        onChange={handlePasswordChange}
+        value={loginForm.password}
+        onChange={handleLoginFormChange}
       />
-      <button type='submit'>로그인</button>
+      <button type="submit">로그인</button>
     </form>
   );
 }
---
+++ src/lettrip-web/Service/APIService.js
@@ -0,0 +1,53 @@
+import { Axios } from "axios";
+import { API_BASE_URL, ACCESS_TOKEN } from "../constant/backendAPI";
+
+export const request = (options) => {
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  if (localStorage.getItem(ACCESS_TOKEN)) {
+    headers.append(
+      "Authorization",
+      "Bearer " + localStorage.getItem(ACCESS_TOKEN)
+    );
+  }
+
+  const defaults = { headers: headers };
+  options = Object.assign({}, defaults, options);
+
+  return fetch(options.url, options).then((response) =>
+    response.json().then((json) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+  );
+};
+
+export function login(loginRequest) {
+  return request({
+    url: API_BASE_URL + "/api/auth/login",
+    method: "POST",
+    body: JSON.stringify(loginRequest),
+  });
+}
+
+export function SignUp(signUpRequest) {
+  return request({
+    url: API_BASE_URL + "/api/auth/sign-up",
+    method: "POST",
+    body: JSON.stringify(signUpRequest),
+  });
+}
+
+export function CreateArticle(articleCreateRequest) {
+  return request({
+    url: API_BASE_URL + "/api/articles/create",
+    method: "POST",
+    body: JSON.stringify(articleCreateRequest),
+  });
+}
+class APIService {}
+export default new APIService();
