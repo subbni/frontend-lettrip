9a5d9c9aa3aa49d708234ab81b7e87c796b4dcab
subbni <slfkak@naver.com>
2023-06-06 15:21:06 +0900

    미션 - 랭킹 뷰 (w. Swiper) 구현

--- src/lettrip-web/Component/Mission/RankingContainer.js
+++ src/lettrip-web/Component/Mission/RankingContainer.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 import RankingItem from "./RankingItem";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+SwiperCore.use([Navigation, Pagination, Autoplay]); // Swiper
 
 const RankingContainer = () => {
   const rankingNameList = [
@@ -39,22 +46,39 @@ const RankingContainer = () => {
     return formattedDate;
   };
 
+  const now = new Date();
   const from = getFirstDate(new Date());
   const to = getLastDate(new Date());
 
+  const swiperStyle = {
+    position: "relative",
+    height: "22em",
+    width: "58em",
+  };
   return (
     <div>
-      <div className='ranking_title'>진행중인 랭킹</div>
+      <div className='ranking_title'>{now.getMonth() + 1}월 진행 중인 랭킹</div>
       <div className='ranking_container'>
-        {rankingNameList.map((rankingName, idx) => (
-          <RankingItem
-            key={idx}
-            rankingName={rankingName}
-            rankingType={rankingTypeList[idx]}
-            from={from}
-            to={to}
-          />
-        ))}
+        <Swiper
+          className='ranking_swiper'
+          style={swiperStyle}
+          spaceBetween={40}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          {rankingNameList.map((rankingName, idx) => (
+            <SwiperSlide>
+              <RankingItem
+                key={idx}
+                rankingName={rankingName}
+                rankingType={rankingTypeList[idx]}
+                from={from}
+                to={to}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
