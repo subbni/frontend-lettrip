4edea1f699f7ca7c85fc69d649a8cd06ec06808e
jini814 <rose7734@naver.com>
2023-06-08 10:55:54 +0900

    장소 검색 : 별점 및 뱃지 적용 완료

--- src/lettrip-web/Component/PlaceReview/PlaceSearchForm.js
+++ src/lettrip-web/Component/PlaceReview/PlaceSearchForm.js
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import SearchMapForm from "./SearchMapForm";
 import { RiArrowGoBackLine } from "react-icons/ri";
 import { getPlace, getReviewPage } from "../../Service/PlaceReviewService";
+
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import medal from "../../../image/medal.png";
+
+import "./PlaceReview.css";
+
 const PlaceSearchForm = ({ onGetResults }) => {
   const [place, setPlace] = useState({
     name: "",
@@ -12,6 +18,7 @@ const PlaceSearchForm = ({ onGetResults }) => {
     city: "",
   });
   const [totalRating, setTotalRating] = useState(-1);
+  const [soloFriendlyTotalRating, setSoloFriendlyTotalRating] = useState(1);
 
   const [pageForm, setPageForm] = useState({
     page: 0,
@@ -36,6 +43,7 @@ const PlaceSearchForm = ({ onGetResults }) => {
     getPlace(place.xpoint, place.ypoint)
       .then((response) => {
         console.log(response);
+        setSoloFriendlyTotalRating(response.soloFriendlyTotalRating);
         if (response.errorCode) {
         } else {
           setTotalRating(response.totalRating);
@@ -70,21 +78,71 @@ const PlaceSearchForm = ({ onGetResults }) => {
     });
   };
 
+  // 별점 관리
+  const renderStars = () => {
+    const rating = totalRating;
+
+    const filledStar = <FaStar className='searchresult-ratingStarIcon' />;
+    const halfStar = <FaStarHalfAlt className='searchresult-ratingStarIcon' />;
+    const emptyStar = <FaRegStar className='searchresult-ratingStarIcon' />;
+
+    const stars = [];
+    const integerPart = Math.floor(rating); // 정수 부분
+    const decimalPart = rating % 1; // 소수 부분
+
+    // 꽉 찬 별
+    for (let i = 0; i < integerPart; i++) {
+      stars.push(filledStar);
+    }
+    // 반 별
+    if (decimalPart >= 0.5) {
+      stars.push(halfStar);
+    }
+    // 빈 별
+    const remainingStars = 5 - stars.length;
+    for (let i = 0; i < remainingStars; i++) {
+      stars.push(emptyStar);
+    }
+    return stars;
+  };
+
+  // 메달 관리
+  const totalSoloRating = soloFriendlyTotalRating;
+
   return (
-    <div className='place_searchForm'>
-      {isPlaceSelected ? (
-        <div>
-          <div className='place_name'>{place.name}</div>
-          <div className='place_category'>{place.categoryName}</div>
-          <div>{totalRating}점</div>
-          <div className='place_research_btn'>
-            <RiArrowGoBackLine onClick={onReSearchBtnClick} />
-            <span className='place_research_msg'>다시 검색</span>
+    <div>
+      <div className='place_searchForm'>
+        {isPlaceSelected ? (
+          <div>
+            <div className='place_name'>{place.name}</div>
+            {totalSoloRating >= 0.5 && (
+              <div className='place-medal'>
+                <img className='place-medal-image' src={medal} />
+                <p className='recommend-place-solo-travel'>
+                  과반수가 혼자 가기 추천한 여행지
+                </p>
+              </div>
+            )}
+            <div className='place_category'>{place.categoryName}</div>
+
+            <div className='place_rating'>
+              <span className='place-ratingStars'>
+                {renderStars().map((star, index) => (
+                  <span key={index}>{star}</span>
+                ))}
+              </span>
+              {totalRating}점
+            </div>
+
+            <div className='place_research_btn'>
+              <RiArrowGoBackLine onClick={onReSearchBtnClick} />
+              <span className='place_research_msg'>다시 검색</span>
+            </div>
           </div>
-        </div>
-      ) : (
-        <SearchMapForm onPlaceSelect={onPlaceSelect} />
-      )}
+        ) : (
+          <SearchMapForm onPlaceSelect={onPlaceSelect} />
+        )}
+      </div>
     </div>
   );
 };
